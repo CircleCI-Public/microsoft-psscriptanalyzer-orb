7274f86c4231532d1d889fa6d9a041a94c05fa61
Gabriel Simmer <gabriel@circleci.com>
2021-05-21 12:11:19 +0100

    Testing different nuget install.

--- src/scripts/install.ps1
+++ src/scripts/install.ps1
@@ -4,7 +4,8 @@ if ($version -gt "5.1.14393.206") {
     # installed. 
     # As per https://github.com/PowerShell/PSScriptAnalyzer#from-powershell-gallery.
     Write-Output "Installing Nuget (required for your version of PowerShell)"
+    Register-PackageSource -provider NuGet -name nugetRepository -location https://www.nuget.org/api/v2
     Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 –Force
 }
 
-Install-Module -Name PSScriptAnalyzer -RequiredVersion $Env.PSSA_INSTALL_VERSION
+Install-Module -Name PSScriptAnalyzer -RequiredVersion $Env.PSSA_INSTALL_VERSION -Force

ab5ff9d2110cfe9d45794f56354f74bef962f52c
Gabriel Simmer <gabriel@circleci.com>
2021-05-21 15:18:31 +0100

    Remove nuget.

--- src/scripts/install.ps1
+++ src/scripts/install.ps1
@@ -1,13 +1,2 @@
-$version = (Get-Host).Version.ToString()
-if ($version -gt "5.1.14393.206") {
-    # If we're running a newer version of PowerShell, ensure latest Nuget is 
-    # installed. 
-    # As per https://github.com/PowerShell/PSScriptAnalyzer#from-powershell-gallery.
-    Write-Output "Installing Nuget (required for your version of PowerShell)"
-    Find-PackageProvider -Name "Nuget" -AllVersions
-    Install-PackageProvider -Name "Nuget" -RequiredVersion "2.8.5.201" -Force
-    Get-PackageProvider -ListAvailable
-    Import-PackageProvider -Name "Nuget" -RequiredVersion "2.8.5.201" -Verbose
-}
-
+Register-PSRepository -Default
 Install-Module -Name PSScriptAnalyzer -RequiredVersion $Env:PSSA_INSTALL_VERSION -Force

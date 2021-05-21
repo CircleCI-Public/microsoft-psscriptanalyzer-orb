c9087dc20238d145f28a562ce1d9f1e9454071dd
Gabriel Simmer <gabriel@circleci.com>
2021-05-21 12:22:09 +0100

    Casing.

--- src/scripts/install.ps1
+++ src/scripts/install.ps1
@@ -1,12 +1,10 @@
 $version = (Get-Host).Version.ToString()
 if ($version -gt "5.1.14393.206") {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     # If we're running a newer version of PowerShell, ensure latest Nuget is 
     # installed. 
     # As per https://github.com/PowerShell/PSScriptAnalyzer#from-powershell-gallery.
     Write-Output "Installing Nuget (required for your version of PowerShell)"
-    Register-PackageSource -provider NuGet -name nugetRepository -location https://www.nuget.org/api/v2
-    Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 –Force
+    Install-PackageProvider NuGet -MinimumVersion 2.8.5.201 –Force
 }
 
 Install-Module -Name PSScriptAnalyzer -RequiredVersion $Env:PSSA_INSTALL_VERSION -Force

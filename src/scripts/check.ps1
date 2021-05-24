6533300b870833d42ab24ba3091a339f1f208472
Gabriel Simmer <gabriel@circleci.com>
2021-05-24 09:28:50 +0100

    Verbose for debugging.

--- src/scripts/check.ps1
+++ src/scripts/check.ps1
@@ -1,5 +1,5 @@
 if ($Env:PSSA_CHECK_RECURSE -eq 1) {
-    Invoke-ScriptAnalyzer -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS -Recurse
+    Invoke-ScriptAnalyzer -v -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS -Recurse
 } else {
-    Invoke-ScriptAnalyzer -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS
+    Invoke-ScriptAnalyzer -v -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS
 }

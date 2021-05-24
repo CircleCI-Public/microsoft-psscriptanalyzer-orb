3cbe8ee66c3e4197cbe9f1ace82eafbf88072af5
Gabriel Simmer <gabriel@circleci.com>
2021-05-24 09:43:04 +0100

    Add sample dir.

--- src/scripts/check.ps1
+++ src/scripts/check.ps1
@@ -1,5 +1,5 @@
 if ($Env:PSSA_CHECK_RECURSE -eq 1) {
-    Invoke-ScriptAnalyzer -v -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS -Recurse
+    Invoke-ScriptAnalyzer -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS -Recurse
 } else {
-    Invoke-ScriptAnalyzer -v -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS
+    Invoke-ScriptAnalyzer -Path $Env:PSSA_CHECK_MODULE -Settings $Env:PSSA_CHECK_SETTINGS
 }
